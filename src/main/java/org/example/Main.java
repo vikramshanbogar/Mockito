cf1c4f0436a2e4d69721d6e0721c21a2268a9fc6
Vikram Shanbogar <70604290+vikramshanbogar@users.noreply.github.com>
2024-06-20 23:35:30 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-//        Operations operations = new Operations();
-//        System.out.println(operations.OnCOndition(p -> p.getAmount() > 4000));
+        Operations operations = new Operations(null);
+        System.out.println(operations.OnCOndition(p -> p.getAmount() > 4000));
     }
 }
